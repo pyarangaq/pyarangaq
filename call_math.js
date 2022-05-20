3342ea2594c2fa5693dfdfa3026d1e9490d3ecde
Percy Yaranga <pyarangaq@ruraytelecom.com.pe>
2022-05-19 19:03:39 -0500

    Creating Branch2

--- call_math.js
+++ call_math.js
@@ -1,8 +1,9 @@
 const math = require("./math.js");
 
-console.log(math);
-
 /*
+console.log(math);
+*/
+
+
 console.log(math.add(2,1));
 console.log(math.sus(2,1));
-*/
--- math.js
+++ math.js
@@ -6,5 +6,6 @@ function sus(x1,x2){
     return x1-x2;
 }
 
+
 exports.add = add;
 exports.sus = sus;
